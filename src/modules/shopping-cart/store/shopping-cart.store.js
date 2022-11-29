996724738abcb5ea2a1d09b1c9e9761d70a5f54c
Mohamed Omar <femohamedomar@globaldirections.net>
2022-11-29 14:54:18 +0200

    feat: :sparkles: add pinia action to add item to shopping cart and local storage

--- src/modules/shopping-cart/store/shopping-cart.store.js
+++ src/modules/shopping-cart/store/shopping-cart.store.js
@@ -26,5 +26,28 @@ export default defineStore("cart", {
     },
   },
 
-  actions: {},
+  actions: {
+    // action to add an item to the cart
+    addToCart(item) {
+      let cart = this.cart;
+
+      let found = cart.find((product) => product.id === item.id);
+
+      // if the item is already in the cart, increase the quantity
+      if (found) {
+        found.quantity++;
+      } else {
+        // if the item is not in the cart, add it
+        cart.push({
+          ...item,
+          quantity: 1,
+        });
+      }
+
+      this.cart = cart;
+
+      // save the cart to the local storage
+      localStorage.setItem("cart", JSON.stringify(cart));
+    },
+  },
 });
